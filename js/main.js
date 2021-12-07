b09dcdaae8ef111c276b562bc704265d4640df3d
spainfrap <82938109+spainfrap@users.noreply.github.com>
2021-12-07 23:34:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('.rev_slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+        // variableWidth: true
+    });
+
+});
